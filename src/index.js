7e5202e9c9b6907852053e79ac572f82028b5670
Alberto Reina <ajreina@uvg.edu.gt>
2021-06-24 12:57:28 -0600

    fixed bug dob

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ const userRouter = require('./routes/user')
 const taskRouter = require('./routes/task')
 
 const corsOptions = {
-    origin: "*", methods:
-        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS", allowedHeaders:
-        "Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Origin,Cache-Control,Content-Type,X-Token,X-Refresh-Token", credentials: true, preflightContinue: false,
-    optionsSuccessStatus: 204
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"
 };
 
 const app = express()
