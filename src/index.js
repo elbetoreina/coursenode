c220ccc6f9af6a1de3962e1f798739e9362d5271
Alberto Reina <ajreina@uvg.edu.gt>
2021-06-23 21:15:07 -0600

    DOB Finished

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ require('./db/mongoose')
 const userRouter = require('./routes/user')
 const taskRouter = require('./routes/task')
 
+// const corsOptions = {
+//     origin: "*", methods:
+//         "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS", allowedHeaders:
+//         "Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Origin,Cache-Control,Content-Type,X-Token,X-Refresh-Token", credentials: true, preflightContinue: false,
+//     optionsSuccessStatus: 204
+// };
 const app = express()
 app.use(cors())
 const port = process.env.PORT
--- src/routes/user.js
+++ src/routes/user.js
@@ -90,7 +90,7 @@ router.get('/users/me', auth, async (req, res) => {
 
 router.patch('/users/me', auth, async (req, res) => {
 
-    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const allowedUpdates = ['name', 'email', 'password', 'age', 'dob']
     const updates = Object.keys(req.body)
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
